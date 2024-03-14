21bdc7751ab18a82194ca23b901bc97d5fa8b5ce
Maksims Grisunins <maksgris@gmail.com>
2024-03-14 14:28:59 +0200

    Initial commit

---
+++ src/main/java/edu/rtu/lv/bachmicroservicespringboot/BachMicroserviceSpringBootApplication.java
@@ -0,0 +1,13 @@
+package edu.rtu.lv.bachmicroservicespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachMicroserviceSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BachMicroserviceSpringBootApplication.class, args);
+	}
+
+}
